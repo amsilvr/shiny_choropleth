7f7ae18db5ef25793ac739ee37b8f695b66a07dd
amsilvr <silverman.aaron@gmail.com>
2017-05-06 01:53:42 -0400

    Working with normal projection - restores PR - also doesn't need to re-merge each time you select a different type

--- CMAS_Clean_shiny.R
+++ CMAS_Clean_shiny.R
@@ -4,13 +4,20 @@ require(googlesheets)
 require(tidyverse)
 require(lubridate)
 require(stringr)
-
+require(sf)
+#require(rgdal)
 
 ss_new <- gs_key("1Xw4JefUCS4HHQ0KpvKhr-DjklqzhH3_CeA-zhoAuQfI", visibility = "private") #CMAS_Alerts_Processed
 day_file_name <- paste0(today(),"-msgfile.csv")
 
 load_msgs <- function() {
-        msg <- gs_read(ss = ss_new
+        
+   if (file.exists(day_file_name)) {
+     msg <- read_csv(day_file_name) %>% select(-X1)
+   } 
+   
+  else   
+    msg <-  gs_read_csv(ss = ss_new
                        , col_names = c("rec_time", "cmac", "full_text")
                        , coltypes = "Tcc", skip = 1, trim_ws = TRUE) %>%
                 mutate(rec_time = mdy_hms(gsub(" at ", " ", rec_time)
@@ -73,19 +80,72 @@ load_msgs <- function() {
        return(msg)
        
         } 
+# State and Territory Lookup
 
+map_states <- function() {
+  state_url <- "http://www2.census.gov/geo/tiger/GENZ2016/shp/cb_2016_us_state_20m.zip"
+} 
+
+state_iso <- read_csv("https://www2.census.gov/geo/docs/reference/codes/files/national_county.txt"
+                      , col_names = c("iso_3166_2"
+                                      , "STATEFP"
+                                      , "COUNTYFP"
+                                      , "name","x")) %>%
+    select(2,1) %>%
+    unique()
+
+
+map_counties <- function() {
+  # Download Shapefiles
+  
+  countyshapes_url <- "http://www2.census.gov/geo/tiger/GENZ2016/shp/cb_2016_us_county_20m.zip"
+  if (!dir.exists("data")) {dir.create("data")}
+  if (!file.exists("data/county_shape_file.zip")) {
+    download.file(countyshapes_url
+                  , destfile = "data/county_shape_file.zip")
+  }
+    t <- unzip("data/county_shape_file.zip", exdir = "data")
+   
+ 
+   # Read the file with sf
+tmp <- st_read(t[4]) %>%
+  left_join(state_iso) %>%
+  group_by(STATEFP, COUNTYFP)
+  tmp$NAME <- str_replace_all(tmp$NAME, pattern = "Ã±",replacement = "n") %>%
+  str_replace_all("Ã¡",replacement = "a") %>%
+  str_replace_all("Ã¼",replacement = "u") %>%
+  str_replace_all("Ã³",replacement = "o") %>%
+  str_replace_all("Ã",replacement = "i")
+  return(tmp)
+}
+  
 
 ## Download local copy of FIPS lookup data and read into memory
 load_fips <- function() {
-        
-  fips_lookup <- albersusa::counties_sf() %>%
-        transmute(fips, name, lsad
-                  , state, abb = iso_3166_2
-                  , value = paste(abb, name))
-}
-
+  counties_sf <- map_counties() %>%
+    mutate(areaname = paste(iso_3166_2, NAME)) %>%
+    return()
+  
+  }
 ## isolates the state and county, given column with 
 ## comma separated list in format "city (ST)"
+# 
+# right_join(areas) %>%
+#   select(msg_id, GEOID) %>%
+#   rbind(areas_states)
+
+lsad_lookup <- function() {
+  url <- "https://www.census.gov/geo/reference/lsad.html"
+  lsad <- htmltab::htmltab(doc = url, which = "//th[text() = 'LSAD']/ancestor::table") %>%
+    filter(grepl("06|04|12|05|03|00|15|25|13", LSAD) == TRUE) %>%
+    transmute(LSAD,description = `LSAD Description`) %>%
+    mutate(description = 
+                str_extract(pattern = "^[^(]*",string = description) %>%
+                str_trim()) %>%
+    replace_na(list(LSAD = "", description = "")) %>%
+    filter(!description == "Balance of County EC Place")
+  
+}
 
 area_find <- function(area_list) { 
         area_list <- str_replace_all(area_list
@@ -102,7 +162,7 @@ area_find <- function(area_list) {
         area_clean <- paste(n[[1]][,2]
                             , str_trim(m[[1]][,1], side = "both")) %>%
         
-  # ## Clean TCS county names to match list from albersusa county map
+  # ## Clean TCS county names to match list from census county map
   # ##       
         str_replace_all(pattern = "E\\.",replacement = "East") %>%
         str_replace_all(pattern = "W\\.",replacement = "West") %>%
@@ -120,22 +180,23 @@ area_find <- function(area_list) {
 ## For areas that include only state names
 
 full_state <- function(areas_states) {
-        if (!exists("fips_lookup")) fips_lookup <- load_fips()
-         left_join(areas_states, fips_lookup, by = c("areas" = "abb")) %>%
-         select(msg_id,fips) %>%
-         return()
+  if (!exists("fips_lookup")) fips_lookup <- load_fips()
+  
+          left_join(areas_states, fips_lookup, by = c("areas" = "iso_3166_2")) %>%
+        select(msg_id,GEOID) %>%
+        return()
 }
 
 flatten_fips <- function(msg) {
-  
-  areas <- select(msg, msg_id, areas)
+  if (!exists("fips_lookup")) fips_lookup <- load_fips()
+  areas <- transmute(msg
+                     , msg_id = as.character(msg_id)
+                     , areas)
   #separate out alerts with full state areas, convert directly to fips
   areas_states <- filter(areas, str_length(areas) == 2) %>%
     full_state()
   #remove those alerts from the other areas
   areas <- filter(areas, str_length(areas) > 2)
-  
-  if (!exists("fips_lookup")) fips_lookup <- load_fips()
   
   # create a matrix of areas for each message id that has individual counties
   areas <- tapply(areas$areas, area_find
@@ -145,13 +206,40 @@ flatten_fips <- function(msg) {
       unlist(recursive = TRUE) %>%
       as_tibble(validate = TRUE) %>%
       rownames_to_column() %>%
-      transmute(msg_id = str_extract(rowname, "[[:alnum:]]{8}")
-                                     , value) 
-
-  fips_msg <- inner_join(areas, fips_lookup) %>%
-          select(msg_id, fips) %>%
-          rbind(areas_states)
-
+      transmute(msg_id = as.character(str_extract(rowname, "[[:alnum:]]{8}"))
+                                     , areaname = value) %>%
+  # Join messages with FIPS codes by matching areanames
+  
+    left_join(fips_lookup) %>%
+    transmute(msg_id, areaname, GEOID = as.character(GEOID))
+  
+  # Fix the 18 that don't seem to match for whatever reason
+  areas <-  mutate(areas, GEOID = 
+             case_when(
+                      grepl("MD Baltimore city",areas$areaname) ~ "24005",
+                      grepl("SD Shannon",areas$areaname) ~ "46113",
+                      grepl("TX Wall",areas$areaname) ~ "48473",
+                      grepl("NV Carson",areas$areaname) ~ "32510",
+                      grepl("PR Rio Grande",areas$areaname) ~ "72119",
+                      grepl("PR Manati",areas$areaname) ~ "72091",
+                      grepl("PR Juana Diaz",areas$areaname) ~ "72075",
+                      grepl("PR Loiza",areas$areaname) ~ "72087",
+                      grepl("LA La Salle",areas$areaname) ~ "22059",
+                      grepl("SD Pennington city",areas$areaname) ~ "46103",
+                      grepl("PR Las Marias",areas$areaname) ~ "72083",
+                      grepl("CA Lake city",areas$areaname) ~ "06033",
+                      grepl("PR Comerio",areas$areaname) ~ "72045",
+                      grepl("KY Carter city",areas$areaname) ~ "21043",
+                      grepl("VA Roanoke city",areas$areaname) ~ "51770",
+                      grepl("MN McLeod city",areas$areaname) ~ "27085",
+                      TRUE ~ areas$GEOID
+                )
+      )    %>%
+    select(-areaname) %>%
+    rbind(areas_states) %>%
+    return()
+ 
+  
 }
 
 # Classify message type - 
@@ -168,13 +256,14 @@ classify_message <- function(msg) {
              grepl("Tsunami", msg$message_text, ignore.case = TRUE) ~ "Tsunami",
              TRUE ~ "Other")
   ) %>%
-    transmute(msg_id
+    transmute(msg_id = as.character(msg_id)
               , rec_time
               , expire_time
               , response = response_type
               , urgency
               , wea = message_text 
               , type = as.factor(type)
+              , areas
     ) 
 }
 
@@ -189,21 +278,29 @@ classify_message <- function(msg) {
 # from the google sheet
 
 if (!exists("msg")) {
-  #if (file.exists(day_file_name)) msg <- read.csv(day_file_name)
-  #else
+  if (file.exists(day_file_name)) {
+      msg <- read.csv(day_file_name)
+      }
+  else
     msg <- load_msgs()
   }
 
-if (!exists("fips_msg")){
-   fips_msg <- flatten_fips(msg)
-}
 
 msg2 <- classify_message(msg) %>% unique()
 
+if (!exists("fips_msg")) {
+   fips_msg <- flatten_fips(msg2)
+}
+
+
 alert_tally <- left_join(msg2, fips_msg) %>%
-        select(msg_id, fips, type) %>%
-        count(fips, type) %>%
+        select(msg_id, GEOID, type) %>%
+        group_by(GEOID, type) %>%
+        tally() %>%
         rename(WEATYPE = type, WEANUM = n) %>%
-        spread(WEATYPE, WEANUM, fill = "0",drop = TRUE, convert = TRUE) 
+        spread(WEATYPE, WEANUM, fill = "0",drop = TRUE, convert = TRUE) %>%
+        mutate(Total = AMBER + FlashFlood
+               + Other + Tornado
+               + Tsunami)
 
         
--- app.R
+++ app.R
@@ -10,49 +10,79 @@
 
 
 require(shiny)
+require(shinythemes)
 require(tidyverse)
 require(lubridate)
 require(stringr)
 require(sf)
 require(leaflet)
-require(albersusa)
 
-source('CMAS_Clean_shiny.R', echo = TRUE)
+# Download Shapefiles
+# 
+if (!exists("alert_tally")) {
+  source("CMAS_Clean_shiny.R", echo = TRUE)
+}
+
+countyshapes_url <- "http://www2.census.gov/geo/tiger/GENZ2016/shp/cb_2016_us_county_20m.zip"
+stateshapes_url <- "http://www2.census.gov/geo/tiger/GENZ2016/shp/cb_2016_us_state_20m.zip"
+if (!dir.exists("data")) {dir.create("data")}
+if (!file.exists("data/county_shape_file.zip")) {
+  download.file(countyshapes_url
+                , destfile = "data/county_shape_file.zip")}
+
+if (!file.exists("data/state_shape_file.zip")) {
+  download.file(stateshapes_url
+                , destfile = "data/state_shape_file.zip")}
+  
+  c_shp <- unzip("data/county_shape_file.zip", exdir = "data")
+  s_shp <- unzip("data/state_shape_file.zip", exdir = "data")
+
+# Read the file with sf and add the proper crs code for this projection
+
+counties_sf <- read_sf(c_shp[5]) %>%
+  left_join(state_iso) %>%
+  st_transform('+proj=longlat +datum=WGS84') %>%
+  inner_join(lsad_lookup())
+counties_sf$NAME <- str_replace_all(counties_sf$NAME, pattern = "Ã±",replacement = "ñ") %>%
+  str_replace_all("Ã¡",replacement = "á") %>%
+  str_replace_all("Ã¼",replacement = "ñ") %>%
+  str_replace_all("Ã³",replacement = "ó") %>%
+  str_replace_all("Ã",replacement = "í")
+
+states_sf <- read_sf(s_shp[4]) %>% 
+  st_transform('+proj=longlat +datum=WGS84')
 
 
-alert_tally <- mutate(alert_tally
-                      , Total = AMBER + FlashFlood
-                              + Other + Tornado
-                              + Tsunami)
-# EPSG Definition from
-# setView( 39.50 , -98.35, zoom = 5) %>%
-epsg2163 <- leafletCRS(
-  crsClass = "L.Proj.CRS",
-  code = "EPSG:2163",
-  proj4def = "+proj=laea +lat_0=38.393632 +lon_0=-101.8458245 +x_0=0 +y_0=0
-              +a=6370997 +b=6370997 +units=m +no_defs",
-  resolutions = 2^(16:7))
+bins <- c(0, 1, 3, 5, 10, 20, 30, 40, 80, 205)
+pal <- colorBin("Reds", domain = NULL, bins = bins, pretty = TRUE)
 
-state_sf <- usa_sf()
-counties_sf <- counties_sf()
-
-bins <- c(0, 1, 5, 10, 15, 20, 25, 30, 300)
-pal <- colorBin("Blues", domain = NULL, bins = bins, pretty = TRUE)
 
 # Define UI for application that draws a histogram
 ui <- bootstrapPage(
+  theme = shinytheme('superhero'),
 
    # Application title
-  tags$style(type="text/css", "html, body {width:100%;height:100%;text-align:center;}"),
+  tags$style(type = "text/css",
+          "html,
+             body {width:100%;height:100%;text-align:center;} 
+             .selectize-input { font-size: 32px; background-color:aqua; }
+             .selectize-dropdown { font-size: 25px; line-height: 30px; }
+             .control-label { font-size: 32px; color: black!important;} 
+             .shiny-input-container {background-color:white;}
+             .content::-webkit-scrollbar {display: none;"),
 
-  h1("WARN Alerts by County: mouse over map for more info"),
+  h1("WARN Alerts by County"),
+  h3("Mouse over map for more info"),
 
   # Show choropleth of selected alerts
   leafletOutput("map", width = "100%", height = "100%"),
 
-   # Sidebar with a radio buttons input for types of alerts
-   absolutePanel(top = 10, right = 10, draggable = TRUE,
-        selectInput(inputId = "alertType", label = "Which Alert Type?"
+   # Dropdown menu
+   absolutePanel( top = "20px"
+                 , left = "10%"
+                 , width = "20%"
+                 , draggable = TRUE
+        ,selectInput(inputId = "alertType", label = "Which Alert Type?"
                      ,choices = c("Total"
                                   ,"AMBER"
                                   ,"FlashFlood"
@@ -61,43 +91,60 @@ ui <- bootstrapPage(
                                   ,"Other")
               )
       )
-
-
-
 )
+
+allCounties <- left_join(counties_sf, alert_tally)
+allCounties[is.na(allCounties)] <- 0
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
   type <- reactive(renderText(input$alertType))
   # map data
   fd <- reactive({
-    inner_join(counties_sf
-              ,select(alert_tally
-                    , inst = matches(input$alertType))
-                    )
+    allCounties %>%
+              mutate_(inst = input$alertType)
         })
 
+
 output$map <- renderLeaflet({
-    leaflet(data = fd()
-            , options = leafletOptions(crs = epsg2163)) %>%
-
-
-      addPolygons(stroke = FALSE
-                , label = ~paste0(name
+    leaflet(data = fd()) %>%
+      addTiles(
+        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+        attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
+        ) %>%
+      setView(lng = -93.85, lat = 37.45, zoom = 5) %>%
+    
+      addPolygons(data = states_sf
+                , stroke = TRUE
+                , weight = 2
+                , opacity = 1
+                , color = "grey"
+                , fill = FALSE
+    ) %>%
+    addPolygons(data = fd()
+                #, layerId = input$alertType
+                , stroke = FALSE
+                , label = ~paste0("<strong>"
+                                  ,NAME
                                   ," "
-                                  ,lsad
+                                  ,description #lookup table for lsad
                                   ,", "
                                   ,iso_3166_2
-                                  ,": "
-                                  ,inst)
+                                  ,":</strong><br />"
+                                  ,inst
+                                  ," "
+                                  ,input$alertType
+                                  ," WEA Messages") %>% 
+                  lapply(htmltools::HTML)
                 , labelOptions = labelOptions(style = list(
-                        "color" = "steelblue",
-                        "font-style" = "bold",
-                        "box-shadow" = "3px 3px rgba(0,0,0,0.25)",
-                        "font-size" = "15px",
-                        "border-color" = "rgba(0,0,0,0.5)"))
+                      "color" = "steelblue",
+                      "box-shadow" = "3px 3px rgba(0,0,0,0.25)",
+                      "text-align" = "left",
+                      "font-size" = "15px",
+                      "border-color" = "rgba(0,0,0,0.5)"))
                 , fillOpacity = .5
-                , smoothFactor = 1
+                , smoothFactor = .5
                 , fillColor = ~pal(inst)
                 , highlight = highlightOptions(
                       weight = 5,
@@ -105,15 +152,29 @@ output$map <- renderLeaflet({
                       dashArray = "",
                       fillOpacity = 1,
                       bringToFront = FALSE)
-                ) %>%
-            addPolygons(data = state_sf
-                        ,stroke = TRUE
-                        , weight = 2
-                        , opacity = 1
-                        , color = "grey"
-                        , fill = FALSE
-            )
-    })
+        )
+})
+  
+  # A reactive expression that returns the set of counties that are
+  # in bounds right now - adapted from https://github.com/rstudio/shiny-examples/blob/master/063-superzip-example/server.R
+  # # countiesInBounds <- reactive({
+  # #   if (is.null(input$map_bounds))
+  # #     return(allCounties[FALSE,])
+  # #   bounds <- input$map_bounds
+  # #   latRng <- range(bounds$north, bounds$south)
+  # #   lngRng <- range(bounds$east, bounds$west)
+  # #   
+  # #   subset(allCounties,
+  # #          latitude >= latRng[1] & latitude <= latRng[2] &
+  # #            longitude >= lngRng[1] & longitude <= lngRng[2])
+  # })
+  
+    
+
+ 
+ # observe({# new polygon layer
+ #   proxy <- leafletProxy("map") 
+ #   })
 
  observe({ # redraw legend
       proxy <- leafletProxy("map", data = fd()) %>%
