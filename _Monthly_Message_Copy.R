d5b1204498fca4a357d139ebd3ada380ca179275
amsilvr <silverman.aaron@gmail.com>
2017-05-11 13:33:03 -0400

    Added googlesheets script to this directory

---
+++ _Monthly_Message_Copy.R
@@ -0,0 +1,22 @@
+# Get all new WEA messages and move to 
+# "CMAS_Alerts_Processed" sheet
+# Suggested monthly run
+
+library(googlesheets)
+require(tidyverse)
+require(lubridate)
+
+##
+
+
+NewCMASImport <- function() { #copies new messages into main sheet
+        raw <- gs_key("1GnchiRm2TXgQ1TpTGcsCIGggIjEIsd6TeuVyY_s4a3U") #CMAS Alerts
+        full <- gs_key("1Xw4JefUCS4HHQ0KpvKhr-DjklqzhH3_CeA-zhoAuQfI") #CMAS_Alerts_Processed
+        msg <- gs_read(raw)
+        msg <- tail(msg, 240)
+        gs_add_row(ss = full, ws = 1, input = msg, verbose = TRUE) -> howmany 
+        print(length(howmany))
+    }    
+
+
+
